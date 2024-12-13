a3f9d9a2e07ca89f1307920d251c3484d68ca6b2
abin98anto <abin98anto@gmail.com>
2024-12-13 09:16:10 +0530

    Before signup feature

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,14 @@ import CoursePage from "./pages/user/CoursesPage/CoursesPage";
 import AboutPage from "./pages/user/AboutPage/AboutPage";
 import SubscriptionPage from "./pages/user/SubscriptionPage/SubscriptionPage";
 import SignupPage from "./pages/user/SignupPage/SignupPage";
+import LoginPage from "./pages/user/LoginPage/LoginPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<UserLayout />}>
       <Route path="/" element={<LandingPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/courses" element={<CoursePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/subscriptions" element={<SubscriptionPage />} />
