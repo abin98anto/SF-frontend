ac2957874893524df8bbb91bd7c615707254be0d
abin98anto <abin98anto@gmail.com>
2024-12-13 09:30:20 +0530

    Starting Signup Process -cleaned up the sign up page using constants. -next to do the sign up functionality.

--- src/pages/user/SignupPage/SignupPage.tsx
+++ src/pages/user/SignupPage/SignupPage.tsx
@@ -38,23 +38,23 @@ const SignupPage = () => {
               style={{ display: "none" }}
             />
             <button className="button" onClick={triggerFileInput} type="button">
-              <svg xmlns="http://www.w3.org/2000/svg">
+              <svg xmlns={imageLinks.GOOGLE_SVG}>
                 <rect className="border" pathLength={100} />
                 <rect className="loading" pathLength={100} />
                 <svg
                   className="done-svg"
-                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns={imageLinks.GOOGLE_SVG}
                   viewBox="0 0 24 24"
                 >
                   <path
                     className="done done-cloud"
                     pathLength={100}
-                    d="M 6.5,20 Q 4.22,20 2.61,18.43 1,16.85 1,14.58 1,12.63 2.17,11.1 3.35,9.57 5.25,9.15 5.88,6.85 7.75,5.43 9.63,4 12,4 14.93,4 16.96,6.04 19,8.07 19,11 q 1.73,0.2 2.86,1.5 1.14,1.28 1.14,3 0,1.88 -1.31,3.19 Q 20.38,20 18.5,20 Z"
+                    d={imageLinks.D_CLOUD}
                   />
                   <path
                     className="done done-check"
                     pathLength={100}
-                    d="M 7.515,12.74 10.34143,15.563569 15.275,10.625"
+                    d={imageLinks.D_CHECK}
                   />
                 </svg>
               </svg>
@@ -79,29 +79,12 @@ const SignupPage = () => {
                 viewBox="0 0 48 48"
                 height="1em"
                 width="1em"
-                xmlns="http://www.w3.org/2000/svg"
+                xmlns={imageLinks.GOOGLE_SVG}
               >
-                <path
-                  fill="#FFC107"
-                  d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
-      	c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
-      	c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-                />
-                <path
-                  fill="#FF3D00"
-                  d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
-      	C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-                />
-                <path
-                  fill="#4CAF50"
-                  d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
-      	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-                />
-                <path
-                  fill="#1976D2"
-                  d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
-      	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-                />
+                <path fill="#FFC107" d={imageLinks.GOOGLE_SIGN1} />
+                <path fill="#FF3D00" d={imageLinks.GOOGLE_SIGN2} />
+                <path fill="#4CAF50" d={imageLinks.GOOGLE_SIGN3} />
+                <path fill="#1976D2" d={imageLinks.GOOGLE_SIGN4} />
               </svg>
               <span>Log in with Google</span>
             </div>
