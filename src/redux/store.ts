a8a58ac75a7cebed75cefbf850adc68aad00f837
abin98anto <abin98anto@gmail.com>
2024-12-19 00:28:07 +0530

    redux/store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,36 +1,30 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
-import userReducer from "./features/userSlice";
-import userAuthReducer from "./features/userAuthSlice";
+import userReducer from "./features/user/userSlice";
+import userAuthReducer from "./features/user/userAuthSlice";
 import tutorReducer from "./features/tutor/tutorSlice";
-// import tutorAuthReducer from "./features/tutor/tutorAuthSlice";
 import adminAuthReducer from "./features/admin/adminAuthSlice";
-import userListReducer from "./features/userListSlice";
+import userListReducer from "./features/user/userListSlice";
 import tutorListReducer from "./features/tutor/tutorListSlice";
 
-// Persist configuration
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["user", "userLogin", "adminLogin"],
 };
 
-// Combine all reducers
 const rootReducer = combineReducers({
   user: userReducer,
   userLogin: userAuthReducer,
   userList: userListReducer,
   tutor: tutorReducer,
-  // tutorLogin: tutorAuthReducer,
   tutorList: tutorListReducer,
   adminLogin: adminAuthReducer,
 });
 
-// Wrap rootReducer with persistReducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Configure the store
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
